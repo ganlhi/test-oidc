a8306e7a11ff743ef3d7e947d3a46b706285aee7
Guilhem Brouat <guilhem.brouat@docdoku.com>
2018-05-31 10:59:18 +0200

    Updates

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,12 +1,12 @@
 import { UserManagerSettings, WebStorageStateStore } from 'oidc-client';
 
 export const clientSettings: UserManagerSettings = {
-  authority: 'http://det1ihdpapp02.int.tdc:10414',
-  client_id: 'MyClient3',
+  authority: 'http://localhost:10414',
+  client_id: 'MyClient',
   redirect_uri: 'http://localhost:4200/auth-callback',
   post_logout_redirect_uri: 'http://localhost:4200',
   response_type: 'id_token token',
-  scope: 'openid profile MyClient3.Api',
+  scope: 'openid profile MyClient.Api',
   filterProtocolClaims: true,
   loadUserInfo: true,
   userStore: new WebStorageStateStore({ store: window.localStorage }),
